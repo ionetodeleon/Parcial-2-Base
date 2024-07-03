f21807a717696de96b5cb7b8063e749ac50a52bb
Ignacio Oneto <ionetodeleon@gmail.com>
2024-07-03 17:51:31 -0300

    Add project files.

---
+++ Parcial 2/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Parcial_2.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> opciones) : base(opciones)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+}
